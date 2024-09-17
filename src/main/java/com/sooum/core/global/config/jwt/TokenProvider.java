446c16d4c8e2f774985ac0aae902ae84458ebedb
koungq <guu119@naver.com>
2024-09-17 16:39:09 +0900

    feat: jwt

---
+++ src/main/java/com/sooum/core/global/config/jwt/TokenProvider.java
@@ -0,0 +1,108 @@
+package com.sooum.core.global.config.jwt;
+
+import com.sooum.core.domain.member.dto.AuthDTO.Token;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+import static org.hibernate.query.sqm.tree.SqmNode.log;
+
+@Service
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    private final JwtProperties jwtProperties;
+
+    private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+    private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+    private static final String ID_CLAIM = "id";
+
+    public Token createToken(Long id) {
+        return new Token(
+                createAccessToken(id),
+                createRefreshToken(id)
+        );
+    }
+
+    public String createAccessToken(Long id) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getAccessTokenExpirationPeriod()))
+                .setSubject(ACCESS_TOKEN_SUBJECT)
+                .claim(ID_CLAIM, id)
+                .signWith(Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String createRefreshToken(Long id) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + jwtProperties.getAccessTokenExpirationPeriod()))
+                .setSubject(REFRESH_TOKEN_SUBJECT)
+                .claim(ID_CLAIM, id)
+                .signWith(Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Boolean validateToken(String jwtToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(jwtToken);  // Decode
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Object detailedValidateToken(String jwtToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(jwtToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.info("JWT token has expired."); // 유효기간 만료
+            return e;
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT token: " + e.getMessage());   // 지원하지 않는 형식 또는 구조
+            return e;
+        } catch (MalformedJwtException e) {
+            log.info("Invalid JWT token: " + e.getMessage()); // 지원하지 않는 형식 또는 손상
+            return e;
+        } catch (SignatureException e) {
+            log.info("Invalid JWT signature: " + e.getMessage()); // 유효하지 않은 서명
+            return e;
+        } catch (JwtException e) {
+            log.info("Invalid JWT token: " + e.getMessage()); // 예상하지 못한 유효성 검사 실패
+            return e;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getToken(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+        return new UsernamePasswordAuthenticationToken(new User(claims.get(ID_CLAIM, String.class), "", authorities), token, authorities);
+    }
+
+    private Claims getToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(jwtProperties.getKey().getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
