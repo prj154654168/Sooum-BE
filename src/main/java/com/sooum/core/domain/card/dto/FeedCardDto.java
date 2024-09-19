486ee6b37c368f30f638f1e16d60ce7568f1db18
jiixon <jr0107380@gmail.com>
2024-09-19 19:00:55 +0900

    refactor: feedcarddto

--- src/main/java/com/sooum/core/domain/card/dto/FeedCardDto.java
+++ src/main/java/com/sooum/core/domain/card/dto/FeedCardDto.java
@@ -1,8 +1,46 @@
 package com.sooum.core.domain.card.dto;
 
-import com.sooum.core.global.responseform.ResponseEntityModel;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sooum.core.domain.card.entity.font.Font;
+import com.sooum.core.domain.card.entity.fontsize.FontSize;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 
+import java.time.LocalDateTime;
+
+@Setter
 @Getter
-public class FeedCardDto extends ResponseEntityModel<FeedCardDto> {
+public abstract class FeedCardDto extends RepresentationModel<FeedCardDto> {
+    private long id;
+    @JsonProperty(value = "isStory")
+    private boolean isStory;
+    private LocalDateTime storyExpirationTime;
+    private String content;
+    private LocalDateTime createdAt;
+    private int likeCnt;
+    @JsonProperty(value = "isLiked")
+    private boolean isLiked;
+    private int commentCnt;
+    @JsonProperty(value = "isCommentWritten")
+    private boolean isCommentWritten;
+    private Link backgroundImgUrl;
+    private Font font;
+    private FontSize fontSize;
+
+    public FeedCardDto(long id, boolean isStory, LocalDateTime storyExpirationTime, String content, LocalDateTime createdAt, int likeCnt, boolean isLiked, int commentCnt, boolean isCommentWritten, Link backgroundImgUrl, Font font, FontSize fontSize) {
+        this.id = id;
+        this.isStory = isStory;
+        this.storyExpirationTime = storyExpirationTime;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.likeCnt = likeCnt;
+        this.isLiked = isLiked;
+        this.commentCnt = commentCnt;
+        this.isCommentWritten = isCommentWritten;
+        this.backgroundImgUrl = backgroundImgUrl;
+        this.font = font;
+        this.fontSize = fontSize;
+    }
 }
