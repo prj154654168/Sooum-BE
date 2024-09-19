11ec6c232bf906786f3646873d97882386fe57c9
LeeKing2017 <thrusum123@gmail.com>
2024-09-20 05:28:42 +0900

    refactor<PopularFeedRepository>: 인기글 조회 query 수정 * jasypt 삭제 * application.yml에 jasypt 암호화 제거 * test package도 application.yml 따로 관리하도록 생성

--- src/test/java/com/sooum/core/CoreApplicationTests.java
+++ src/test/java/com/sooum/core/CoreApplicationTests.java
@@ -3,10 +3,8 @@ package com.sooum.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CoreApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
--- src/test/java/com/sooum/core/domain/card/controller/PopularFeedControllerTest.java
+++ src/test/java/com/sooum/core/domain/card/controller/PopularFeedControllerTest.java
@@ -5,47 +5,38 @@ import com.sooum.core.domain.card.dto.popularitytype.PopularityType;
 import com.sooum.core.domain.card.entity.font.Font;
 import com.sooum.core.domain.card.entity.fontsize.FontSize;
 import com.sooum.core.domain.card.service.PopularFeedService;
-import org.junit.jupiter.api.BeforeEach;
+import com.sooum.core.global.auth.interceptor.JwtBlacklistInterceptor;
+import com.sooum.core.global.config.jwt.TokenProvider;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.context.WebApplicationContext;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(PopularFeedControllerTest.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@MockBean(JwtBlacklistInterceptor.class)
+@MockBean(TokenProvider.class)
 class PopularFeedControllerTest {
     @Autowired
     MockMvc mockMvc;
-    @Autowired
-    private WebApplicationContext context;
     @MockBean
     PopularFeedService popularFeedService;
-
-    @BeforeEach
-    public void init(){
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-    }
+    private static final String ACCESS_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjY3NzIyOTUsImV4cCI6MTcyNjc3MzI5NSwic3ViIjoiUmVmcmVzaFRva2VuIiwiaWQiOjYyNDY4MDc0NTU1MDc1ODUyMn0.4Ehm0097eiEl9ShS3FVvOlNjnl_5RYcX1ExCWeTNmoU";
 
     @Test
     @WithMockUser
@@ -55,12 +46,32 @@ class PopularFeedControllerTest {
 
         // when
         ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get("/cards/home/popular")
+                MockMvcRequestBuilders
+                        .get("/cards/home/popular")
+                        .header("Authorization", ACCESS_TOKEN)
         );
 
         // then
-//        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
-        // todo security 포함한 테스트로 변경
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        resultActions.andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithMockUser
+    void findEmptyFeed() throws Exception{
+        /// given
+        given(popularFeedService.findHomePopularFeeds(any(), any(), any())).willReturn(List.of());
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .get("/cards/home/popular")
+                        .header("Authorization", ACCESS_TOKEN)
+        );
+
+        // then
+        // todo mocking 안되는 버그 수정
+//        resultActions.andExpect(MockMvcResultMatchers.status().isNoContent());
         resultActions.andDo(MockMvcResultHandlers.print());
     }
 
@@ -81,11 +92,10 @@ class PopularFeedControllerTest {
                     .isCommentWritten(false)
                     .commentCnt(10)
                     .popularityType(PopularityType.LIKE)
-                    .build();
+                    .build()
+                    .add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(FeedCardController.class).findFeedCardInfo((long) i)).withRel("detail"));
             responses.add(dto);
         }
         return responses;
     }
-
-
 }
