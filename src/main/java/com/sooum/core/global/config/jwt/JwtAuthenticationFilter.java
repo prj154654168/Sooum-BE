94c05b5a21bb9860474dbde61f67d7886634e177
koungq <guu119@naver.com>
2024-09-23 00:08:06 +0900

    refactor: check accessToken role

--- src/main/java/com/sooum/core/global/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sooum/core/global/config/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.sooum.core.global.config.jwt;
 
+import com.sooum.core.domain.member.entity.Role;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,20 +18,30 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
-    private final static String TOKEN_PREFIX = "Bearer ";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         tokenProvider.getRefreshToken(request).ifPresentOrElse(this::setAuthentication,
-                () -> tokenProvider.getAccessToken(request).ifPresent(this::setAuthentication));
+                () -> tokenProvider.getAccessToken(request).ifPresent(accessToken -> {
+                    if (!isBanned(accessToken)) {
+                        setAuthentication(accessToken);
+                    }
+                }));
 
         filterChain.doFilter(request, response);
     }
 
-    private void setAuthentication(String refreshToken) {
-        if (tokenProvider.validateToken(refreshToken)) {
-            Authentication authentication = tokenProvider.getAuthentication(refreshToken);
+    private void setAuthentication(String token) {
+        if (tokenProvider.validateToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+    }
+
+    private boolean isBanned(String token) {
+        Role tokenRole = tokenProvider.getRole(token)
+                .orElse(Role.BANNED);   // 토큰에 role이 비워져 있다면 비정상적 요청이므로 BAN 상태로 처리하여 접근 제어
+
+        return tokenRole == Role.BANNED;
     }
 }
