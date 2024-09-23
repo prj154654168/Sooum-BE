8b7cc53f0e8ed3c010bea3578a0c0b8f86f2e017
jiixon <jr0107380@gmail.com>
2024-09-23 16:21:47 +0900

    feat: saveBlockMember 서비스 구현

--- src/main/java/com/sooum/core/domain/block/service/BlockMemberService.java
+++ src/main/java/com/sooum/core/domain/block/service/BlockMemberService.java
@@ -1,6 +1,12 @@
 package com.sooum.core.domain.block.service;
 
+import com.sooum.core.domain.block.entity.Block;
 import com.sooum.core.domain.block.repository.BlockRepository;
+import com.sooum.core.domain.member.entity.Member;
+import com.sooum.core.domain.member.service.MemberService;
+import com.sooum.core.global.exceptionmessage.ExceptionMessage;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +16,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BlockMemberService {
     private final BlockRepository blockRepository;
+    private final MemberService memberService;
 
     public List<Long> findAllBlockToPk(Long memberPk) {
         return blockRepository.findAllBlockToPk(memberPk);
+    }
+
+    public void saveBlockMember(Long fromMemberPk, Long toMemberPk) {
+        if(blockRepository.existsByFromMemberPkAndToMemberPk(fromMemberPk, toMemberPk)){
+            throw new EntityExistsException(ExceptionMessage.ALREDY_BLOCKD.getMessage());
+        }
+
+        Member fromMember = memberService.findByPk(fromMemberPk);
+        Member toMember = memberService.findByPk(toMemberPk);
+
+        blockRepository.save(Block.builder()
+                .toMember(toMember)
+                .fromMember(fromMember).build());
     }
 }
