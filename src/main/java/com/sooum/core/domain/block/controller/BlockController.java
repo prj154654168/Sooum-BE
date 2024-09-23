4b9dca38314ee2aa7c737f6cde76782d23de1b23
jiixon <jr0107380@gmail.com>
2024-09-23 16:58:21 +0900

    refactor: toMemberPk PathVariable -> Dto로 변경

--- src/main/java/com/sooum/core/domain/block/controller/BlockController.java
+++ src/main/java/com/sooum/core/domain/block/controller/BlockController.java
@@ -1,29 +1,28 @@
 package com.sooum.core.domain.block.controller;
 
+import com.sooum.core.domain.block.dto.BlockDto;
 import com.sooum.core.domain.block.service.BlockMemberService;
 import com.sooum.core.global.auth.annotation.CurrentUser;
 import com.sooum.core.global.responseform.ResponseStatus;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
-@RequestMapping("/cards/blocks")
+@RequestMapping("/block")
 @RequiredArgsConstructor
 public class BlockController {
     private final BlockMemberService blockMemberService;
 
-    @PostMapping("/{toMemberPk}")
-    ResponseEntity<ResponseStatus> blockMember(@PathVariable Long toMemberPk, @CurrentUser Long fromMemberPk) {
-        blockMemberService.saveBlockMember(fromMemberPk, toMemberPk);
+    @PostMapping
+    ResponseEntity<ResponseStatus> blockMember(@RequestBody @Valid BlockDto blockDto, @CurrentUser Long fromMemberPk) {
+        blockMemberService.saveBlockMember(fromMemberPk, blockDto.toMemberPk());
 
-        return ResponseEntity.created(URI.create("/blocks/" + toMemberPk))
+        return ResponseEntity.created(URI.create("/block/" + blockDto.toMemberPk()))
                 .body(ResponseStatus.builder()
                         .httpCode(HttpStatus.CREATED.value())
                         .httpStatus(HttpStatus.CREATED)
