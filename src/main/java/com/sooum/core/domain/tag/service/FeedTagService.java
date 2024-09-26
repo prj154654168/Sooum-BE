24f4a907cd6e0771fe02a8554924d6214212e24d
jiixon <jr0107380@gmail.com>
2024-09-26 17:49:25 +0900

    feat: TagService 구현

--- src/main/java/com/sooum/core/domain/tag/service/FeedTagService.java
+++ src/main/java/com/sooum/core/domain/tag/service/FeedTagService.java
@@ -1,20 +1,14 @@
 package com.sooum.core.domain.tag.service;
 
-import com.sooum.core.domain.card.controller.FeedCardController;
 import com.sooum.core.domain.card.entity.FeedCard;
 import com.sooum.core.domain.tag.dto.TagDto;
 import com.sooum.core.domain.tag.entity.Tag;
 import com.sooum.core.domain.tag.repository.FeedTagRepository;
 import com.sooum.core.global.util.NextPageLinkGenerator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 @RequiredArgsConstructor
@@ -23,27 +17,13 @@ public class FeedTagService {
 
     public List<TagDto.ReadTagResponse> readTags(FeedCard feedCard) {
         List<Tag> tagsByFeedCard = getTagsByFeedCard(feedCard);
-//        return NextPageLinkGenerator.generateTagFeedLink(tagsByFeedCard.stream()
-//                .map(tag -> TagDto.ReadTagResponse.builder()
-//                        .id(tag.getPk())
-//                        .content(tag.getContent())
-//                        .build()
-//                )
-//                .toList());
-        return tagsByFeedCard.stream()
-                .map(tag -> {
-                    TagDto.ReadTagResponse tagResponse = TagDto.ReadTagResponse.builder()
-                            .id(tag.getPk())
-                            .content(tag.getContent())
-                            .build();
-                    tagResponse.add(
-                            linkTo(FeedCardController.class).slash("tags/" + tag.getPk()).withRel("tag-feed")
-                    );
-                    return tagResponse;
-                })
-                .collect(Collectors.toList());
-
-
+        return NextPageLinkGenerator.appendEachTagDetailLink(
+                tagsByFeedCard.stream()
+                        .map(tag -> TagDto.ReadTagResponse.builder()
+                                .id(tag.getPk())
+                                .content(tag.getContent())
+                                .build())
+                        .toList());
     }
 
     public List<Tag> getTagsByFeedCard(FeedCard feedCard) {
