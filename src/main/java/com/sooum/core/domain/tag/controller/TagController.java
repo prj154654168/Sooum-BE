1d7608cd5e961f898dd1734b2b3786e012b593b1
jiixon <jr0107380@gmail.com>
2024-09-26 17:50:26 +0900

    feat: 임시 TagController 구현

--- src/main/java/com/sooum/core/domain/tag/controller/TagController.java
+++ src/main/java/com/sooum/core/domain/tag/controller/TagController.java
@@ -1,4 +1,20 @@
 package com.sooum.core.domain.tag.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tags")
 public class TagController {
+
+    @GetMapping("/{tagPk}")
+    ResponseEntity<?> findFeedsByTag(@PathVariable("tagPk") Long tagPk) {
+        //TODO: 추후 태그별 피드조회 구현
+        return null;
+    }
 }
