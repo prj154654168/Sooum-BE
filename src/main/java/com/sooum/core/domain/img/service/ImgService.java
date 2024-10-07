81940485c857d14dafa4686da9038fbfb44a986c
jang-199 <jw1010110@gmail.com>
2024-10-07 19:08:20 +0900

    refactor: Service 어노테이션 잘못 추가되어 삭제

--- src/main/java/com/sooum/core/domain/img/service/ImgService.java
+++ src/main/java/com/sooum/core/domain/img/service/ImgService.java
@@ -3,14 +3,9 @@ package com.sooum.core.domain.img.service;
 import com.sooum.core.domain.card.entity.imgtype.ImgType;
 import com.sooum.core.domain.img.dto.ImgUrlInfo;
 import org.springframework.hateoas.Link;
-import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.rekognition.model.DetectModerationLabelsRequest;
-import software.amazon.awssdk.services.rekognition.model.Image;
-import software.amazon.awssdk.services.rekognition.model.ModerationLabel;
-import software.amazon.awssdk.services.rekognition.model.S3Object;
 
 import java.util.List;
-@Service
+
 public interface ImgService {
     Link findImgUrl(ImgType imgType, String imgName);
     List<ImgUrlInfo> createDefaultImgRetrieveUrls(List<String> previousImgsName);
