d31949599e713d08d868e55578aa1603b83d44af
koungq <guu119@naver.com>
2024-10-10 17:30:34 +0900

    refactor: resolve serialize error

--- src/main/java/com/sooum/core/domain/member/entity/Blacklist.java
+++ src/main/java/com/sooum/core/domain/member/entity/Blacklist.java
@@ -1,5 +1,9 @@
 package com.sooum.core.domain.member.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.sooum.core.domain.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -21,6 +25,8 @@ public class Blacklist extends BaseEntity {
     private String token;
 
     @Column(name = "EXPIRED_AT")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime expiredAt;
 
     @Builder
