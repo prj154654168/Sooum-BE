1f6b7bf83aa0e7ae24414eaff03465da2cfb7746
jang-199 <jw1010110@gmail.com>
2024-10-12 21:45:12 +0900

    profile url 변경

--- src/main/java/com/sooum/core/domain/member/controller/ProfileController.java
+++ src/main/java/com/sooum/core/domain/member/controller/ProfileController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/profile")
+@RequestMapping("/profiles")
 @RequiredArgsConstructor
 public class ProfileController {
 
