bce2a7729e6607e2d0d59a174154974f199f2a45
jiixon <jr0107380@gmail.com>
2024-10-17 21:13:16 +0900

    refactor: controller 변경

---
+++ src/main/java/com/sooum/core/domain/member/controller/MemberController.java
@@ -0,0 +1,46 @@
+package com.sooum.core.domain.member.controller;
+
+import com.sooum.core.domain.card.dto.MyCommentCardDto;
+import com.sooum.core.domain.card.service.CommentInfoService;
+import com.sooum.core.global.auth.annotation.CurrentUser;
+import com.sooum.core.global.responseform.ResponseCollectionModel;
+import com.sooum.core.global.responseform.ResponseStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping("/members")
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final CommentInfoService commentInfoService;
+
+    @GetMapping("/comment-cards")
+    public ResponseEntity<?> getMyCommentsCards(@CurrentUser Long memberPk,
+                                                @RequestParam(required = false) Optional<Long> lastId) {
+        List<MyCommentCardDto> myCommentCards = commentInfoService.getMyCommentCards(memberPk, lastId);
+
+        if (myCommentCards.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(ResponseCollectionModel.<MyCommentCardDto>builder()
+                .status(ResponseStatus.builder()
+                        .httpCode(HttpStatus.OK.value())
+                        .httpStatus(HttpStatus.OK)
+                        .responseMessage("Successfully retrieved all your comment cards.")
+                        .build()
+                )
+                .content(myCommentCards)
+                .build()
+        );
+    }
+
+}
