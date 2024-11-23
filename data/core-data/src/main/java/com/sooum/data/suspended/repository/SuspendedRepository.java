b130269aa54f951f4e382f09d3d0e37d1b02eee8
jiixon <jr0107380@gmail.com>
2024-11-23 23:31:28 +0900

    feat: repository 구현

--- data/core-data/src/main/java/com/sooum/data/suspended/repository/SuspendedRepository.java
+++ data/core-data/src/main/java/com/sooum/data/suspended/repository/SuspendedRepository.java
@@ -3,5 +3,8 @@ package com.sooum.data.suspended.repository;
 import com.sooum.data.suspended.entity.Suspended;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SuspendedRepository extends JpaRepository<Suspended, Long> {
+    Optional<Suspended> findByDeviceId(String deviceId);
 }
