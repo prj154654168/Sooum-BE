1ae742829e914a1f1105db4252fa2635915d7d30
jang-199 <jw1010110@gmail.com>
2024-11-27 20:39:35 +0900

    import 최적화

--- boot/external-api/src/main/java/com/sooum/global/config/security/SecurityConfig.java
+++ boot/external-api/src/main/java/com/sooum/global/config/security/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.sooum.global.config.security;
 
-import com.sooum.global.config.jwt.ExpiredRefreshTokenException;
 import com.sooum.global.config.jwt.JwtAuthenticationFilter;
 import com.sooum.global.config.jwt.TokenProvider;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
