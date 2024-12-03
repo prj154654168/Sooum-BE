87ef0dccea9ddf38e160fbcf0fac5b2859070185
jang-199 <jw1010110@gmail.com>
2024-12-03 19:31:10 +0900

    logging

---
+++ boot/external-api/src/main/java/com/sooum/global/aop/RequesterInfoAspect.java
@@ -0,0 +1,32 @@
+package com.sooum.global.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class RequesterInfoAspect {
+
+    @Before("@within(org.springframework.web.bind.annotation.RestController)")
+    public void before() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        log.info("[Request URI]: {}, [User-Agent]: {}, [Client IP]: {}",
+                request.getRequestURI(),
+                request.getHeader("User-Agent"),
+                ip);
+    }
+}
