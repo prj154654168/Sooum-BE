522d14358a5642c924c882b2d4d3b3092b94fa50
jang-199 <jw1010110@gmail.com>
2024-12-12 16:27:21 +0900

    filter의 url별 http Method 구분을 위해 map으로 변경

--- boot/external-api/src/main/java/com/sooum/global/config/jwt/JwtAuthenticationFilter.java
+++ boot/external-api/src/main/java/com/sooum/global/config/jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.AntPathMatcher;
@@ -13,18 +14,19 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Set;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
-    private final Set<String> EXCLUDE_AUTH_PATH = Set.of(
-            "/users/key",
-            "/users/sign-up",
-            "/users/login",
-            "/members" ,
-            "/profiles/nickname/**/available"
+
+    private final Map<String, HttpMethod> EXCLUDE_AUTH_PATH = Map.of(
+            "/users/key", HttpMethod.GET,
+            "/users/sign-up", HttpMethod.POST,
+            "/users/login", HttpMethod.POST,
+            "/members", HttpMethod.GET,
+            "/profiles/nickname/**/available", HttpMethod.GET
     );
     private static final AntPathMatcher antPathMatcher = new AntPathMatcher();
 
@@ -32,7 +34,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
 
-            if (isExcludedPath(request.getRequestURI())) {
+            if (isExcludedPath(request)) {
                 filterChain.doFilter(request, response);
                 return;
             }
@@ -65,9 +67,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
     }
 
-    public boolean isExcludedPath(String requestPath) {
-        return EXCLUDE_AUTH_PATH.stream()
-                .anyMatch(pattern -> antPathMatcher.match(pattern, requestPath));
+    public boolean isExcludedPath(HttpServletRequest request) {
+        String requestPath = request.getRequestURI();
+        HttpMethod requestMethod = HttpMethod.valueOf(request.getMethod());
+
+        return EXCLUDE_AUTH_PATH.entrySet().stream()
+                .anyMatch(entry -> antPathMatcher.match(entry.getKey(), requestPath)
+                        && entry.getValue().equals(requestMethod));
     }
 
     private void setAuthentication(String token) {
