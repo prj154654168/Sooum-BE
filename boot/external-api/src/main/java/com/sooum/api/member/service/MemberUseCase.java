14f9848c43b925760b941057a00476a8f7534fba
LeeKing2017 <thrusum123@gmail.com>
2024-12-20 00:23:20 +0900

    feat: fcmToken update API 구현

--- boot/external-api/src/main/java/com/sooum/api/member/service/MemberUseCase.java
+++ boot/external-api/src/main/java/com/sooum/api/member/service/MemberUseCase.java
@@ -1,20 +1,26 @@
 package com.sooum.api.member.service;
 
 import com.sooum.api.member.dto.MemberDto;
-import com.sooum.api.member.mapper.MemberMapper;
+import com.sooum.data.member.entity.Member;
 import com.sooum.data.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberUseCase {
     private final MemberService memberService;
-    private final MemberMapper memberMapper;
 
     public MemberDto.MemberStatus findMemberStatus(Long memberPk) {
         return MemberDto.MemberStatus.builder()
                 .banEndAt(memberService.findMember(memberPk).getUntilBan())
                 .build();
+    }
+
+    @Transactional
+    public void updateFCMToken(MemberDto.FCMTokenUpdateRequest requestDto, Long requesterPk) {
+        Member requester = memberService.findMember(requesterPk);
+        requester.updateFCMToken(requestDto.getFcmToken());
     }
 }
