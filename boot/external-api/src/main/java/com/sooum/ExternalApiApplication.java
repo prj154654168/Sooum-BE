6eb2d361c43f2d61ce2f99e95788f3671b7996be
jang-199 <jw1010110@gmail.com>
2025-01-03 21:23:01 +0900

    rsa 스캐줄러 코드 배치 모듈로 이동 및 git action 코드 수정

--- boot/external-api/src/main/java/com/sooum/ExternalApiApplication.java
+++ boot/external-api/src/main/java/com/sooum/ExternalApiApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableScheduling
 @EnableRetry
 public class ExternalApiApplication {
     public static void main(String[] args) {
