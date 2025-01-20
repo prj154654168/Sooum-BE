fbdde006aeddc9dc759c9173fa9ffcba88394a96
jang-199 <jw1010110@gmail.com>
2025-01-20 23:08:05 +0900

    차단 이력 조회 시 차단 기간이 풀렸으면 unban하는 로직 추가

--- boot/external-api/src/main/java/com/sooum/api/member/service/MemberUseCase.java
+++ boot/external-api/src/main/java/com/sooum/api/member/service/MemberUseCase.java
@@ -2,19 +2,27 @@ package com.sooum.api.member.service;
 
 import com.sooum.api.member.dto.MemberDto;
 import com.sooum.data.member.entity.Member;
+import com.sooum.data.member.entity.Role;
 import com.sooum.data.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class MemberUseCase {
     private final MemberService memberService;
 
+    @Transactional
     public MemberDto.MemberStatus findMemberStatus(Long memberPk) {
+        Member member = memberService.findMember(memberPk);
+        if(isBanPeriodExpired(member)){
+            member.unban();
+        }
         return MemberDto.MemberStatus.builder()
-                .banEndAt(memberService.findMember(memberPk).getUntilBan())
+                .banEndAt(member.getUntilBan())
                 .build();
     }
 
@@ -34,5 +42,9 @@ public class MemberUseCase {
         return MemberDto.NotifyAllowResponse.builder()
                 .isAllowNotify(memberService.findMemberNotifyAllow(requesterPk))
                 .build();
+    }
+
+    private static boolean isBanPeriodExpired(Member member) {
+        return member.getRole() == Role.BANNED && member.getUntilBan().isBefore(LocalDateTime.now());
     }
 }
