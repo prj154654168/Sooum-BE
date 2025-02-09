ae7e3175b08a631a8bc022b43051b819d1d18e56
jang-199 <jw1010110@gmail.com>
2025-02-09 11:42:19 +0900

    Rsa key가 업데이트 되지 않아도 가장 마지막 키를 반환하고 slack을 전송하도록 이벤트 리스너 구현

--- data/core-data/src/main/java/com/sooum/data/rsa/service/RsaService.java
+++ data/core-data/src/main/java/com/sooum/data/rsa/service/RsaService.java
@@ -1,19 +1,20 @@
 package com.sooum.data.rsa.service;
 
+import com.sooum.data.common.event.SlackEvent;
 import com.sooum.data.rsa.entity.Rsa;
 import com.sooum.data.rsa.repository.RsaRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RsaService {
-
+    private final ApplicationEventPublisher eventPublisher;
     private final RsaRepository rsaRepository;
 
 
@@ -31,12 +32,20 @@ public class RsaService {
         rsaRepository.deleteExpiredKey(LocalDateTime.now());
     }
 
-    public Rsa findRsaKey() {
-        List<Rsa> rsa = rsaRepository.findRsa(LocalDateTime.now(), PageRequest.ofSize(1));
-        if (rsa.isEmpty()) {
-            throw new IllegalArgumentException("rsa key가 없습니다 findRsaKey()");}
-        return rsa.get(0);
+    public Rsa findLatestRsaKey() {
+        Optional<Rsa> latestRsaKey = rsaRepository.findLatestRsaKey(LocalDateTime.now());
+        return latestRsaKey.orElseGet(this::findLastRsaKey);
     }
 
+    private Rsa findLastRsaKey() {
+        Optional<Rsa> lastGeneratedRsaKey = rsaRepository.findLastGeneratedRsaKey();
 
+        if (lastGeneratedRsaKey.isEmpty()) {
+            throw new IllegalArgumentException("Rsa 테이블이 비어있습니다.");
+        }
+        eventPublisher.publishEvent(SlackEvent.builder()
+                .eventMsg("Rsa Key가 업데이트 되지 않아 가장 마지막 Rsa Key를 반환하였습니다. Schedule log를 확인해주세요.")
+                .build());
+        return lastGeneratedRsaKey.get();
+    }
 }
