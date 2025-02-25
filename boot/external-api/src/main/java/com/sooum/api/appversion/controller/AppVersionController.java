0037ffb2104316091d6017e95ca0a86a973dbcbf
jang-199 <jw1010110@gmail.com>
2025-02-25 23:18:02 +0900

    앱 버전 상태에 관한 반환 enum 클래스 구현 Cf.PENDING,UPDATE,OK

--- boot/external-api/src/main/java/com/sooum/api/appversion/controller/AppVersionController.java
+++ boot/external-api/src/main/java/com/sooum/api/appversion/controller/AppVersionController.java
@@ -1,11 +1,14 @@
 package com.sooum.api.appversion.controller;
 
+import com.sooum.api.appversion.dto.AppVersionStatus;
+import com.sooum.api.appversion.service.AppVersionService;
 import com.sooum.data.app.repository.AppVersionFlagRepository;
 import com.sooum.data.app.repository.AppVersionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppVersionController {
     private final AppVersionRepository appVersionRepository;
     private final AppVersionFlagRepository appVersionFlagRepository;
+    private final AppVersionService appVersionService;
 
     @GetMapping("/android")
     public ResponseEntity<String> android() {
@@ -28,5 +32,15 @@ public class AppVersionController {
     @GetMapping("/flag")
     public ResponseEntity<Boolean> flag() {
         return ResponseEntity.ok(appVersionFlagRepository.findFlag());
+    }
+
+    @GetMapping("/ios/v2")
+    public ResponseEntity<AppVersionStatus> iosV2(@RequestParam String version) {
+        return ResponseEntity.ok(appVersionService.findIosVersionStatus(version));
+    }
+
+    @GetMapping("/android/v2")
+    public ResponseEntity<AppVersionStatus> androidV2(@RequestParam String version) {
+        return ResponseEntity.ok(appVersionService.findAndroidVersionStatus(version));
     }
 }
---
+++ boot/external-api/src/main/java/com/sooum/api/appversion/dto/AppVersionStatus.java
@@ -0,0 +1,5 @@
+package com.sooum.api.appversion.dto;
+
+public enum AppVersionStatus {
+    PENDING,UPDATE,OK
+}
